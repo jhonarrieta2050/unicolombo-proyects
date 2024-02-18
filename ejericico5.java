11e450437b4b293ce4d308ae92a30f4f81a225ab
Jhon Deivis <jhonarrieta2050@gmail.com>
2024-02-18 18:19:46 -0500

    ejercicio5

---
+++ ejericico5.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ejericico5 {
+    public static void main(String[] args){
+        Scanner lectura = new Scanner(System.in);
+    
+        System.out.println("Digite el diagonal mayor");
+        double dm = lectura.nextDouble();
+        System.out.println("Digite el diagonal menor");
+        double dmn = lectura.nextDouble();
+
+        double area = dm * dmn / 2;
+
+        System.out.println("El area del rombo es de " + area);
+}
+
+}
