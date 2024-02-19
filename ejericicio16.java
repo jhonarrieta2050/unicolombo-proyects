3915db262a576de29d38c55f100f1786bd75887e
Jhon Deivis <jhonarrieta2050@gmail.com>
2024-02-18 20:42:13 -0500

    ejercicio16

---
+++ ejericicio16.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ejericicio16 {
+    public static void main(String[] args){
+        Scanner lectura = new Scanner(System.in);
+
+        System.out.println("Dijite la nota de su examen");
+        double nota = lectura.nextDouble();
+
+        if(nota>3.0){
+            System.out.println("Usted paso el examen");
+        }else{
+            System.out.println("Usted desaprobo el examen");
+        }
+    }
+}
