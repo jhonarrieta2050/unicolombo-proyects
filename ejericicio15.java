cbcdc0d0d9108182e761a7d7070d3c175a5fc7e4
Jhon Deivis <jhonarrieta2050@gmail.com>
2024-02-18 20:39:42 -0500

    ejercici15

---
+++ ejericicio15.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ejericicio15 {
+    public static void main(String[] args){
+        Scanner lectura = new Scanner(System.in);
+
+        System.out.println("Digite su edad");
+        int edad = lectura.nextInt();
+
+        if(edad>18){
+            System.out.println("Usted es mayor de edad");
+        }else{
+            System.out.println("Ustedes es menor de edad");
+        }
+    }
+}
