89a62cfbf5a7c934e8aa40c62949f6280b74c217
Jhon Deivis <jhonarrieta2050@gmail.com>
2024-02-18 20:48:07 -0500

    ejercicio18

---
+++ ejericcio18.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class ejericcio18 {
+    public static void main(String[] args){
+        
+        double cudrado = 125*125;
+
+        System.out.println("El cuadrado de 125 es de " + cudrado);
+
+    }
+}
