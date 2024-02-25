f847d7a26aa635c358c117f39d70533e6f3b0f43
Jhon Deivis <jhonarrieta2050@gmail.com>
2024-02-25 13:41:11 -0500

    ejercicio22

---
+++ ejercico22.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ejercico22 {
+    
+    public static void main(String[]args){
+        Scanner lectura = new Scanner(System.in);
+        int entero = 0, decimal = 0;
+
+        System.out.println("Digite el numero");
+        double numero = lectura.nextDouble();
+        String numeronew =  Double.toString(numero);
+
+        int punto = numeronew.indexOf(".");
+
+        if(punto == -1){
+            entero = Integer.parseInt(numeronew);
+        }else{
+            String enteros = numeronew.substring(0, punto);
+            String decimales = numeronew.substring( punto + 1);
+
+            entero = Integer.parseInt(enteros);
+            decimal = Integer.parseInt(decimales);
+        }
+        
+        System.out.println("La parte entera es de " + entero + " y la decimal es de " + decimal);
+    }
+
+}
