74e90926691f43155febf4dfe1c97cfe51ba4b30
Jhon Deivis <jhonarrieta2050@gmail.com>
2024-02-25 21:36:40 -0500

    ejercicio36

---
+++ ejercicip36.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class ejercicip36 {
+
+    public static void main(String[] args) {
+        
+        Scanner lectura = new Scanner(System.in);
+
+        System.out.println("Write the hours ");
+        int hours = lectura.nextInt();
+        System.out.println("Write the minutes");
+        int minutes = lectura.nextInt();
+
+        double rest1 = hours *15.00;
+        double rest = 15.00/60;
+        double rest2 = minutes * rest;
+        double answer = rest2 + rest1;
+
+
+        System.out.println(answer);
+    }
+    
+    
+}
