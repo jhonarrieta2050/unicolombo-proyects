f4a7d4b1369dfaba9a339ac2fd9797a4bde84725
Jhon Deivis <jhonarrieta2050@gmail.com>
2024-02-25 19:13:48 -0500

    ejercicio29.java

---
+++ ejericcio29.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class ejericcio29 {
+    
+    public static void main(String[] args) {
+        
+        Scanner lectura = new Scanner(System.in);
+
+        System.out.println("Write the number");
+        float number = lectura.nextFloat();
+
+        float answer = number*number*number;
+
+        System.out.println("The answer is " + answer);
+}
+}
+
