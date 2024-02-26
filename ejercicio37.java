e1bf42fb8310433424b172d7044057081d85cd5f
Jhon Deivis <jhonarrieta2050@gmail.com>
2024-02-25 21:41:25 -0500

    ejercicio37

---
+++ ejercicio37.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class ejercicio37 {
+
+    public static void main(String[] args) {
+        
+        Scanner lectura = new Scanner(System.in);
+
+        System.out.println("Write the hours ");
+        int hours = lectura.nextInt();
+        System.out.println("Write the minutes");
+        int minutes = lectura.nextInt();
+
+        
+        
+
+        if(minutes>=15){
+
+            double rest1 = (hours+1 ) *15.00;
+            System.out.println("El cobre es de " + rest1);
+
+        }else{
+            double rest1 = hours *15.00;
+            System.out.println("El cobre es de " + rest1);
+        }
+
+    }
+}
