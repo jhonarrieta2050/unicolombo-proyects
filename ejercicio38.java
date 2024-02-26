3c3e447a326721f4807d2e5e645c1a4cf1693a88
Jhon Deivis <jhonarrieta2050@gmail.com>
2024-02-25 21:49:52 -0500

    ejercicio38

---
+++ ejercicio38.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class ejercicio38 {
+    public static void main(String[] args) {
+        Scanner lectura = new Scanner(System.in);
+        System.out.println("Write your age");
+        int age = lectura.nextInt();
+
+        int gramos = age * 8 * 1000;
+        int manzanas = gramos/4;
+
+        System.out.println("La cantidad de fruta consumida en gramos es de " + gramos);
+        System.out.println("La cantidad de manzanas son de " + manzanas);
+    }
+}
